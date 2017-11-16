bab89d7bedfdf9dc48fb92d8e3190c428163c3df
Dejan Radovanović <deradovanovic@gmail.com>
2017-11-16 21:33:39 +0100

    Calculation operator added

--- calculator.js
+++ calculator.js
@@ -1,10 +1,27 @@
 function calculate() {
     var x, y;
     
-    x = parseInt(document.getElementById("number1").value);
-    y = parseInt(document.getElementById("number2").value);
+    x = parseFloat(document.getElementById("number1").value);
+    y = parseFloat(document.getElementById("number2").value);
 
-    var z = x + y;    
+    var z;
+    var operation = document.getElementById("operation");
+      
+    if (operation.value == '+') {
+        z = x + y;
+    } else if (operation.value == '-') {
+        z = x - y;
+    } else if (operation.value == '/') {
+        if (y == 0 ) {
+            z = "can not be divided by 0"
+        } else {
+            z = x / y;
+        }
+    } else if (operation.value == 'x') {
+        z = x * y;
+    } else {
+        z = 'NaN';
+    }
 
     document.getElementById("result").innerHTML = z;
 }
